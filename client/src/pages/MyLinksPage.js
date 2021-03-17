e4891dbcc76429d74c26d157a56e6d0e07142814
FINNSEEFLY <matmak122@gmail.com>
2021-03-17 04:03:52 +0300

    Initial

---
+++ client/src/pages/MyLinksPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MyLinksPage = () => {
+    document.title = 'Мои ссылки';
+    return (
+        <div>
+            <h1>My Links Page</h1>
+        </div>
+    )
+}
---
+++ routes/linkManage.js
@@ -0,0 +1,64 @@
+const {Router} = require('express')
+const config = require('config')
+const auth = require('../middleware/auth')
+const {insertLinkAndRules} = require("../models/Link");
+const {insertLink, getShortLinkId} = require("../models/Link");
+const {convertUTCTimeToMySqlDateTime} = require('../utils/convertUTCTimeToMySqlDateTime')
+
+const router = Router()
+const systemRegExp = /^((\/)?system)/
+const allowedUrls = /^[\/a-zA-Z0-9_\-%]{1,200}$/
+
+module.exports = router
+
+
+// /system/link/
+router.post(
+    '/create',
+    auth,
+    async (req, res) => {
+        try {
+            let {longUrl, shortUrl, password, clickLimit, disabledOnDateTime} = req.body
+            /*        console.log(`
+                        longUrl=${longUrl}
+                        shortUrl=${shortUrl}
+                        password=${password}
+                        clickLimit=${clickLimit}
+                        disabledOnDateTime=${disabledOnDateTime}
+                    `)
+                    console.log(convertUTCTimeToMySqlDateTime(disabledOnDateTime))*/
+            if (systemRegExp.test(shortUrl)) {
+                res.status(406).json({message: "Недопустимая ссылка /system зарезервирован"})
+                return
+            }
+            if (!allowedUrls.test(shortUrl)) {
+                res.status(406).json({message: "Короткая ссылка содержит недопустимые символы или длиннее 200 символов"})
+                return
+            }
+            if ((await getShortLinkId(shortUrl)).result!=null) {
+                res.status(406).json({message: "Данная короткая ссылка уже существует"})
+                return
+            }
+            if (password!==undefined && password==='') {
+                res.status(406).json({message: "Заполните пароль"})
+                return
+            }
+            if (clickLimit!==undefined && clickLimit<0) {
+                res.status(406).json({message: "Ограничение кликов должно быть положительным"})
+                return
+            }
+            if (disabledOnDateTime!==undefined && disabledOnDateTime==='') {
+                res.status(406).json({message: "Заполните дату и время"})
+                return
+            }
+            if (longUrl.length<=1) {
+                res.status(406).json({message: "Заполните длинную ссылку"})
+                return
+            }
+            await insertLinkAndRules(shortUrl,longUrl,req.user.userId,{password: password, clickLimit: clickLimit, disabledOnDateTime: disabledOnDateTime})
+            res.status(201).json({message:"Ссылка успешно добавлена"})
+        } catch (e) {
+            res.status(500).json({message: "Внутренняя ошибка сервера при создании ссылки"})
+        }
+    }
+)
